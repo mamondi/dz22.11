7ce7b16805e8b3be4ad4dbf56e46237c85aee0d6
DimamonDESKTOP <dimamon02@gmail.com>
2023-12-15 13:39:20 +0200

    done

---
+++ dz22.11/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+void SetCp1251() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
+
+bool isPalindrome(const string& str) {
+    int left = 0, right = str.length() - 1;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+    SetCp1251();
+
+    string inputString;
+    cout << "Введіть рядок: ";
+    getline(cin, inputString);
+
+    if (isPalindrome(inputString)) {
+        cout << "Рядок є паліндромом." << endl;
+    }
+    else {
+        cout << "Рядок не є паліндромом." << endl;
+    }
+
+    return 0;
+}
