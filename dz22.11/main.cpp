9715d2e6bff5b992c23d1c4a6e7aa09d12e773cd
DimamonDESKTOP <dimamon02@gmail.com>
2023-12-15 13:31:45 +0200

    done

---
+++ dz22.11/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+void SetCp1251() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
+
+void replaceSpacesWithTabs(string& str) {
+    for (char& ch : str) {
+        if (ch == ' ') {
+            ch = '\t';
+        }
+    }
+}
+
+int main() {
+    SetCp1251();
+
+    string inputString;
+    cout << "Введіть рядок символів: ";
+    getline(cin, inputString);
+
+    replaceSpacesWithTabs(inputString);
+
+    cout << "Результат: " << inputString << endl;
+
+    return 0;
+}
